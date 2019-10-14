6828017a29404c95d32ecb793e1c274e3a7da77d
Kayden Clark <KaydenClark725@gmail.com>
2019-10-14 16:32:11 -0600

    First Commit

---
+++ index.js
@@ -0,0 +1,10 @@
+
+
+function changeName(event) {
+    var name = document.getElementById("input").value
+    event.preventDefault()
+    document.getElementById("name").innerHTML = name;
+};
+console.log(
+    "hi"
+);
